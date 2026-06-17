c25c9a9c3ae6e293b6bcbb153f05bace258da3e4
GrantPeaster <peastergrant@gmail.com>
2026-06-16 21:36:01 -0400

    Add image rotation functionality to carousel

---
+++ assets/js/carousel.js
@@ -0,0 +1,10 @@
+let index = 0;
+const images = document.querySelectorAll(".carousel-image");
+
+function rotateImages() {
+  images[index].classList.remove("active");
+  index = (index + 1) % images.length;
+  images[index].classList.add("active");
+}
+
+setInterval(rotateImages, 4000);
